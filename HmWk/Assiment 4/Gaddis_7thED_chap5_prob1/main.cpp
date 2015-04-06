9dc10e3ccec40d4c0b0120fe459642efab44e2d4
Joseph-Scuiletti <Joseph.Scuiletti@gmail.com>
2015-04-05 23:14:30 -0700

    Signed-off-by: Joseph-Scuiletti <Joseph.Scuiletti@gmail.com>

---
+++ HmWk/Assiment 4/Gaddis_7thED_chap5_prob1/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * File:   main.cpp
+ * Author: Joseph Scuiletti
+ * Created on April 05,2015,10:40 PM
+ *      Purpose:
+ */
+
+#include <iostream>
+using namespace std;
+
+//Global Constant
+
+//User Libraries
+
+//Function Prototypes
+
+//Excution Begins here
+
+int main(){
+//declare varibles
+unscript int  sum, adnum, total;
+cout<<" this will determine the sum of a through a slected number"<<endl;
+cout<<" please iput the number you want to use"<<endl;
+cin>>sum
+//loop
+for(adnum=1;adnum>=sum;adnumb;){
+	total=adnum+sum;
+	cout<<adnum<<endl;
+	cout<<sum<<endl;
+	cout<<total<<endl;
+	}
+
+    return 0;
+}
+
